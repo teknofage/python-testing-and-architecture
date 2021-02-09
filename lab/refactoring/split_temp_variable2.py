13965bdd3ab8d988d59e593d27bef8028ad6a37b
teknofage <teknofage@gmail.com>
2021-02-09 10:19:28 -0800

    finished exercise 10

--- lab/refactoring/split_temp_variable2.py
+++ lab/refactoring/split_temp_variable2.py
@@ -5,8 +5,8 @@ def save_into_db(info):
     print("saved into databse")
 
 
-user_input = input('Please enter your username: ')
-save_into_db(user_input)
-user_input = int(input('Please enter your birth year: '))
-age = 2020 - user_input
+user_name_input = input('Please enter your username: ')
+save_into_db(user_name_input)
+user_birth_input = int(input('Please enter your birth year: '))
+age = 2020 - user_birth_input
 print("You are",age, "years old.")
