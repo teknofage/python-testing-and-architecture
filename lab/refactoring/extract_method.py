86a5d93d087c57d7385107b5d7db961270ad71de
teknofage <teknofage@gmail.com>
2021-02-09 10:17:35 -0800

    finished exercise 7

--- lab/refactoring/extract_method.py
+++ lab/refactoring/extract_method.py
@@ -1,7 +1,7 @@
 # Written by Kamran Bigdely
 # Example for Compose Methods: Extract Method.
 import math
-
+# GRADE_LIST = []
 
 def print_stat():
     grade_list = []
@@ -9,18 +9,29 @@ def print_stat():
     n_student = 5
     for _ in range(0, n_student):
         grade_list.append(int(input('Enter a number: ')))
+    print(grade_list)
+    return grade_list
 
+def get_mean(grade_list):
     # Calculate the mean and standard deviation of the grades
-    sum = 0  # Do you think 'sum' is a good var name? Run pylint to figure out!
+
+    sum_grades = 0  # Do you think 'sum' is a good var name? Run pylint to figure out!
     for grade in grade_list:
-        sum = sum + grade
-    mean = sum / len(grade_list)
-    sd = 0  # standard deviation
+        sum_grades = sum_grades + grade
+    print(sum_grades / len(grade_list)) 
+    return (sum_grades / len(grade_list))
+    
+def get_sd(grade_list):
+    # sd = 0  # standard deviation
     sum_of_sqrs = 0
     for grade in grade_list:
-        sum_of_sqrs += (grade - mean)**2
-    sd = math.sqrt(sum_of_sqrs / len(grade_list))
+        sum_of_sqrs += (grade - get_mean())**2
+    return math.sqrt(sum_of_sqrs / len(grade_list))
+    
+def print_results():    
     # print out the mean and standard deviation in a nice format.
+    mean = get_mean()
+    sd = get_sd()
     print('****** Grade Statistics ******')
     print("The grades's mean is:", mean)
     print('The population standard deviation of grades is: ', round(sd, 3))
@@ -28,3 +39,6 @@ def print_stat():
 
 
 print_stat()
+get_mean(print_stat)
+get_sd(print_stat)
+print_results()
--- lab/refactoring/inline_method.py
+++ lab/refactoring/inline_method.py
@@ -6,19 +6,13 @@ LEGAL_DRINKING_AGE = 18
 class Person:
     def __init__(self, my_age):
         self.age = my_age
+        print(self.age)
         
-def enter_night_club(individual):
-    if older_than_18_year_old(individual.age):
+def enter_night_club(self):
+    if self.age >= LEGAL_DRINKING_AGE:
         print("Allowed to enter.")
     else:
-        print("Enterance of minors is denited.")
-
-def older_than_18_year_old(age):
-    if age > LEGAL_DRINKING_AGE:
-        return True
-    else: 
-        return False
+        print("Entrance of minors is denied.")
     
-    
-person = Person(17.9)
+person = Person(17)
 enter_night_club(person)
