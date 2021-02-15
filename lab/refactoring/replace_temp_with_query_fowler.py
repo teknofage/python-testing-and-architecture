f3d7520907a88a3147697034cc7f3078b3e27ec6
teknofage <teknofage@gmail.com>
2021-02-15 15:45:48 -0800

    switched replace_temp files back, added split_temporary_variable

--- lab/refactoring/replace_temp_with_query_fowler.py
+++ lab/refactoring/replace_temp_with_query_fowler.py
@@ -1,57 +1,17 @@
 # By Kamran Bigdely Nov. 2020
 # Replace temp variable with query
-# TODO: Use 'extract method' refactoring technique to improve this code. If required, use
-#       'replace temp variable with query' technique to make it easier to extract methods.
-class Employer:    
-    def __init__(self, name):
-        self.name = name
-    def send(self, students):
-        print("Students' contact info were sent to", self.name + '.')
-class Student:
-    def __init__(self, gpa, name):
-        self.gpa = gpa
-        self.name = name
-    def get_gpa(self):
-        return self.gpa
-    def send_congrat_email(self):
-        print("Congrats", self.name + ". You graduated successfully!")
-class School:
-    def __init__(self, students) -> None:
-        self.students = students
-    def process_graduation(self):
-        pass
-    def get_passed_students(self):
-        min_gpa = 2.5 # minimum acceptable GPA
-        passed_students = []
-        for s in self.students:
-            if s.get_gpa() > min_gpa:
-                passed_students.append(s)
-        return passed_students
-    def print_names(self):
-        # print student's name who graduated.
-        print('*** Student who graduated *** ')
-        for s in self.get_passed_students():
-            print(s.name)
-        print('****************************')
-    def send_congrats_email(self):
-        # Send congrat emails to them.
-        for s in self.get_passed_students():
-            s.send_congrat_email()
-    def get_top_10_percent(self):
-        # Find the top 10%
-        self.get_passed_students().sort(key=lambda s: s.get_gpa())
-        percentile = 0.9
-        index = int(percentile * len(self.get_passed_students()))
-        top_10_percent = self.get_passed_students()[index:]
-        return top_10_percent
-    def send_top_to_employers(self):
-        #  Send top 10% contact to the top employers
-        top_employers = [
-            Employer('Microsoft'), Employer('Free Software Foundation'), 
-            Employer('Google')]
-        for e in top_employers:
-            e.send(self.get_top_10_percent())
-students = [Student(2.1, 'donald'), Student(2.3, 'william'), Student(2.7, 'toro'), 
-            Student(3.9, 'lili'), Student(3.2,'kami'), Student(3,'sarah')]
-school  = School(students)
-school.process_graduation()
+def get_base_price(quantity, item_price):
+    base_price = quantity * item_price
+    return base_price
+
+def get_discount(base_price):
+    discount_factor = 0
+    if base_price > 1000: 
+        discount_factor = 0.95
+    else: 
+        discount_factor = 0.98
+    return discount_factor
+
+def get_total(base_price, discount_factor):
+    total = base_price * discount_factor
+    return total
--- lab/refactoring/split_temp_variable.py
+++ lab/refactoring/split_temp_variable.py
@@ -1,17 +1,15 @@
-# by Kami Bigdely
-# Split temporary variable
-
-patty = 70 # [gr]
-pickle = 20 # [gr]
-tomatoes = 25 # [gr]
-lettuce = 15 # [gr]
-buns = 95 # [gr]
-ny_sandwich_weight = (2 * patty + 4 * pickle + 3 * tomatoes + 2 * lettuce
-                + 2 * buns)
-print("NY Burger Weight", ny_sandwich_weight)
-kimchi = 30 # [gr]
-mayo = 5 # [gr]
+patty_weight = 70 # [gr]
+pickle_weight = 20 # [gr]
+tomatoes_weight = 25 # [gr]
+lettuce_weight = 15 # [gr]
+buns_weight = 95 # [gr]
+sandwich_weight = (2 * patty_weight + 4 * pickle_weight + 3 * tomatoes_weight 
+                + 2 * lettuce_weight + 2 * buns_weight)
+print("NY Burger Weight", sandwich_weight)
+kimchi_weight = 30 # [gr]
+mayo_weight = 5 # [gr]
 golden_fried_onion = 20 # [gr]
-seoul_sandwich_weight = (2 * patty + 4 * pickle + 3 * tomatoes 
-                + kimchi + mayo + golden_fried_onion + 2 * buns)
-print("Seoul Kimchi Burger Weight", seoul_sandwich_weight)
+sandwich_weight = (2 * patty_weight + 4 * pickle_weight + 3 * tomatoes_weight 
+                + kimchi_weight + mayo_weight + golden_fried_onion 
+                + 2 * buns_weight)
+print("Seoul Kimchi Burger Weight", sandwich_weight)
