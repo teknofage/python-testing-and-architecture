e9dc27e9be68744f443866cf1677b8c46dc9aa10
teknofage <teknofage@gmail.com>
2021-02-15 16:31:41 -0800

    finished remove_assignment_to_param

--- lab/refactoring/split_temp_variable.py
+++ lab/refactoring/split_temp_variable.py
@@ -5,11 +5,11 @@ lettuce_weight = 15 # [gr]
 buns_weight = 95 # [gr]
 ny_burger_weight = (2 * patty_weight + 4 * pickle_weight + 3 * tomatoes_weight 
                 + 2 * lettuce_weight + 2 * buns_weight)
-print("NY Burger Weight", sandwich_weight)
+print("NY Burger Weight", ny_burger_weight)
 kimchi_weight = 30 # [gr]
 mayo_weight = 5 # [gr]
 golden_fried_onion = 20 # [gr]
 seoul_sandwich_weight = (2 * patty_weight + 4 * pickle_weight + 3 * tomatoes_weight 
                 + kimchi_weight + mayo_weight + golden_fried_onion 
                 + 2 * buns_weight)
-print("Seoul Kimchi Burger Weight", sandwich_weight)
+print("Seoul Kimchi Burger Weight", seoul_sandwich_weight)
