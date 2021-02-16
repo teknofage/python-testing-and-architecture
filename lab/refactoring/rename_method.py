54e6129abf62d896e077f74751b19afe93238da8
teknofage <teknofage@gmail.com>
2021-02-15 16:39:14 -0800

    finished rename_method

--- lab/refactoring/rename_method.py
+++ lab/refactoring/rename_method.py
@@ -1,20 +1,28 @@
-# By Kami Bigdely
-# Introduce explaining variable (alias extract variable)
-# Reference: https://www.researchgate.net/publication/305768969_The_role_of_eye_characteristics_in_facial_beauty_likability_and_attractiveness
-# Background: You are a computer engineer trying to sift through many profiles to find 
-# your soulmate. Unfortunately, there are too many profiles and it's getting tedious. You write a 
-# python script to scrape profiles info (such as height, age, etc) and images (for image processing) 
-# to figure out automatically who is attractive to you.  Here is a part of the script:
-import math
-# assuming you have extracted the following info from the profile's image.
-eye_size = 0.47    # [cm^2]
-eye_width = 24.2   # [mm]
-eye_height = 23.7  # [mm]
+# by Kami Bigdely
+# Rename Method
+# Reference: https://parade.com/1039985/marynliles/pick-up-lines/
 
-iris_width = 20.2  # [mm]
-iris_height = 19.7 # [mm]
+def calculate_area_under_graph(graph):   # TODO: Rename this function to reflect what it's doing.
+    """Calculate the area under the input graph."""
+    # bla bla bla.
+    pass
 
-if eye_size > 0.45 and (math.pi*iris_width/2*iris_height/2) / eye_size >= 0.69 and \
-        eye_height/eye_width >= 0.59:
-    print("I’m sorry I wasn’t part of your past, can I make it up by being in your future?")
-    
+#####################
+
+def find_largest_value_in_list(li):  # TODO: Rename this function to reflect what it's doing.
+    m = li[0]
+    for value in li:
+        if value > m:
+            m = value
+    return m
+
+
+li = [5, -1, 43, 32, 87, -100]
+print(find_largest_value_in_list(li))
+
+############################
+def separate_string_into_words(sentence):  # TODO: Rename this function to reflect what it's doing.
+    words = sentence[0:].split(' ')
+    return words
+
+print(separate_string_into_words('If you were a vegetable, you’d be a ‘cute-cumber.'))
