eab1c7efe9b0b95ff112d4974227dcf9f547eb30
teknofage <teknofage@gmail.com>
2021-02-16 16:22:18 -0800

    updated decompose_conditional2

--- lab/refactoring/decompose_conditional2.py
+++ lab/refactoring/decompose_conditional2.py
@@ -8,15 +8,17 @@ ldl = 30
 triglyceride = 120
 
 def test_cholesterol(total_cholesterol, ldl, triglyceride):
-    if total_cholesterol < 200 and ldl < 100 and triglyceride < 150:
-        # good level
+    good_cholesterol = total_cholesterol < 200 and ldl < 100 and triglyceride < 150
+    high_cholesterol = 200 < total_cholesterol > 240 or ldl > 160 or triglyceride >= 200
+    borderline_cholesterol = 200 <= total_cholesterol <= 240 or 130 <= ldl <= 160 or 150 <= triglyceride < 200
+    if good_cholesterol:   # good level
         print('*** Good level of cholestrol ***')
-    elif 200 < total_cholesterol > 240 or ldl > 160 or triglyceride >= 200:
+    elif high_cholesterol:
         # High cholestrol level
         print('*** High cholestrol level ***')
         print('start taking pills such as statins')
         print('start TLC diet')
-    elif 200 <total_cholesterol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200:
+    elif borderline_cholesterol:
         #TLC_diet
         print('*** Borderline to moderately elevated ***')
         print("Start TLC diet")
@@ -26,3 +28,5 @@ def test_cholesterol(total_cholesterol, ldl, triglyceride):
         print('fruits such as apples, pears, bananas, and oranges.')
     else:
         print('Error: unhandled case.')
+        
+test_cholesterol(total_cholesterol, ldl, triglyceride)
