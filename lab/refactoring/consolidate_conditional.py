3887686c8bebfaf68474c203b1aa8146e2ce6836
teknofage <teknofage@gmail.com>
2021-02-22 13:15:31 -0800

    finished consolidate_conditional

--- lab/refactoring/consolidate_conditional.py
+++ lab/refactoring/consolidate_conditional.py
@@ -2,31 +2,31 @@
 # Consolidate conditional expressions
 def dice(ingredients):
     print("diced all ingredients.")
-def mix_all(diced_ingredients):
+def mix_all(ingredients):
     print("mixed all.")
 def add_salt():
     print('added salt.')
 def pour(liquid):
     print('poured', liquid + '.',)
 
+def lacks_ingredients(ingredients):
+    if 'cucumber' not in ingredients or 'tomato' not in ingredients or 'onion' not in ingredients or 'lemon juice' not in ingredients:
+        print('lacks ingredients.')
+        return True
+    else: 
+        print ('You have all the ingredients.')
+        return False
+    
 def make_shirazi_salad(ingredients):
-    if 'cucumber' not in ingredients:
+    if lacks_ingredients(ingredients) == True:
         print('lacks ingredients.')
-        return
-    if 'tomato' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'onion' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'lemon juice' not in ingredients:
-        print('lacks ingredients.')
-        return
-    dice(ingredients)
-    mix_all(ingredients)
-    add_salt()
-    pour('lemon juice')
-    print('Your yummy shirazi salad is ready!')
+    else: 
+        dice(ingredients)
+        mix_all(ingredients)
+        add_salt()
+        pour('lemon juice')
+        print('Your yummy shirazi salad is ready!')
+    return
 
 if __name__ == "__main__":
     make_shirazi_salad(['cucumber', 'tomato', 'lemon juice', 'onion'])
--- lab/refactoring/decompose_conditional.py
+++ lab/refactoring/decompose_conditional.py
@@ -7,15 +7,21 @@ def make_alert_sound():
 def make_accept_sound():
     print('made acceptance sound')
 
+toxins = ['sodium nitrate', 'sodium benzoate', 'sodium oxide']
 ingredients = ['sodium benzoate']
-if 'sodium nitrate' in ingredients or 'sodium benzoate' in ingredients\
-or 'sodium oxide' in ingredients:
-    print('!!!')
-    print('there is a toxin in the food!')    
-    print('!!!')
-    make_alert_sound()
-else:
-    print('***')
-    print('Toxin Free')
-    print('***')
-    make_accept_sound()
+
+def detect_toxins(toxins, ingredients):
+
+    for i in toxins:
+        if i == ingredients:
+            print('!!!')
+            print('there is a toxin in the food!')    
+            print('!!!')
+            make_alert_sound()
+        else:
+            print('***')
+            print('Toxin Free')
+            print('***')
+            make_accept_sound()
+            
+detect_toxins(toxins, ingredients)
