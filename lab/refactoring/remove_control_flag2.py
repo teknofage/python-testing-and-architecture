89b76b532d830b9f18cf2394415c3c11a78cfb4d
teknofage <teknofage@gmail.com>
2021-02-22 14:02:59 -0800

    finished remove_control_flag2

--- lab/refactoring/remove_control_flag2.py
+++ lab/refactoring/remove_control_flag2.py
@@ -4,11 +4,7 @@
 # This code snippet reads up to the end of the file
 n = 16
 with open('foobar.file', 'rb') as fp:
-    running = True
-    while running:
-        chunk = fp.read(n)
-        if chunk == '': # end of file, stop running.
-            running = False
-        else:
-            print(chunk)
+    chunk = fp.read(n)
+    if chunk != '': # end of file, stop running.
+        print(chunk)
         # process(chunk)
