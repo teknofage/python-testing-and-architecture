1793118a08f37e7f80ac182897d168dea66ba64c
teknofage <teknofage@gmail.com>
2021-02-22 14:05:52 -0800

    remove_control_flag3

--- lab/refactoring/remove_control_flag3.py
+++ lab/refactoring/remove_control_flag3.py
@@ -3,13 +3,11 @@
 # Reference: https://searchcode.com/file/92870153/frameworkconsole/framework.py/
 
 def backdoor_srcmethod():
-    running = True
-    while running:
-        print( "Puts the Android Agent inside an Android App APK. The application runs normally, with extra functionality.")
-        inputfile = input('APK to Backdoor: ').strip()
-        if inputfile == '':
-            running = False
-        else:
-            print('doing other stuff.') 
+    print( "Puts the Android Agent inside an Android App APK. The application runs normally, with extra functionality.")
+    inputfile = input('APK to Backdoor: ').strip()
+    if inputfile == '':
+        return False
+    else:
+        print('doing other stuff.') 
 
 backdoor_srcmethod()
